fc4e489664b8296c24e77f83c3d3885e0661dee9
Lantrol1 <3356895031@qq.com>
2025-11-17 17:50:20 +0800

    11.17 18:00 upd

--- src/core/GameState.cpp
+++ src/core/GameState.cpp
@@ -1,3 +1,38 @@
 //src/core/GameState.cpp
 #pragma once
 #include"GameState.hpp"
+
+GameStateManager& GameStateManager::getInstance() {
+	static GameStateManager instance;
+	return instance;
+}
+
+void GameStateManager::changeState(GameState::State newState) {
+	previousState = currentState;
+	currentState = newState;
+	//上一个状态<-当前状态, 当前状态<-新状态
+	if (newState == GameState::State::PLAYING) {
+		//如果进入新游戏那么重置胜负
+		gameResult = GameState::GameResult::NONE;
+	}
+}
+
+GameState::State GameStateManager::getCurrentState() const {
+	return currentState;
+}
+
+void GameStateManager::setGameResult(GameState::GameResult result) {
+	gameResult = result;
+}
+GameState::GameResult GameStateManager::getGameResult() const {
+	return gameResult;
+}
+
+void GameStateManager::SwitchToPause() {
+	if (currentState == GameState::State::PLAYING) {
+		changeState(GameState::State::PAUSED);
+	}
+	else if (currentState == GameState::State::PAUSED) {
+		changeState(GameState::State::PLAYING);
+	}
+}
--- src/core/GameState.hpp
+++ src/core/GameState.hpp
@@ -1,5 +1,8 @@
 #pragma once
 //定义游戏状态（主菜单/游戏中 or 胜利/失败）
+#include<functional>
+#include<unordered_map>
+
 namespace GameState {
 	enum class State {
 		MAIN_MENU,
@@ -8,9 +11,27 @@ namespace GameState {
 		PAUSED,
 		GAME_OVER
 	};
-	enum class Gameresult {
+	enum class GameResult {
 		NONE,
 		SUCCESS,
 		FAILURE
 	};
 }
+
+class GameStateManager {
+public:
+	static GameStateManager& getInstance();
+	//状态转换
+	void changeState(GameState::State newState);
+	GameState::State getCurrentState() const;
+	//游戏胜负
+	void setGameResult(GameState::GameResult result);
+	GameState::GameResult getGameResult() const;
+	//暂停
+	void SwitchToPause();
+private:
+	GameStateManager() = default;
+	GameState::State currentState = GameState::State::MAIN_MENU;//初始状态在主菜单
+	GameState::State previousState = GameState::State::MAIN_MENU;//初始状态的上一个状态也初始化为主菜单
+	GameState::GameResult gameResult = GameState::GameResult::NONE;//初始胜负未知
+};
